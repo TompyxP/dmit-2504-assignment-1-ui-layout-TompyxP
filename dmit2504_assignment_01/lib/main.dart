03c70a000d3223869ede7f0b39bc3f568a6e83cc
TompyxP <cntompkins245@gmail.com>
2024-01-24 12:41:39 -0700

    assignment 1 finished

--- dmit2504_assignment_01/lib/main.dart
+++ dmit2504_assignment_01/lib/main.dart
@@ -57,7 +57,8 @@ class MyApp extends StatelessWidget {
                             color: Colors.black,
                             width: 3.0,
                           ),
-                        )),
+                        )
+                      ),
                     child: const Center(
                         child: Text(
                       'Container 1',
@@ -66,31 +67,112 @@ class MyApp extends StatelessWidget {
                   )
                 ),
                 Transform.rotate(
-                  angle: pi / 4,
+                    angle: pi / 4,
+                    child: Container(
+                        height: 100.0,
+                        width: 100.0,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          color: Colors.white,
+                        ),
+                        child: const Center(
+                            child: Text(
+                          'Container 2',
+                          textAlign: TextAlign.center,
+                        )
+                      )
+                    )
+                  )
+              ],
+            ),
+            Column(children: [
+              Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
                   child: Container(
                       height: 100.0,
                       width: 100.0,
                       decoration: const BoxDecoration(
                         shape: BoxShape.rectangle,
-                        color: Colors.white,
+                        color: Colors.yellow,
                       ),
-                      child: const Center(
-                          child: Text(
-                        'Container 2',
-                        textAlign: TextAlign.center,
+                      child: const Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Text('Container 3'),
                       )
-                    )
-                  )
-                )
-              ],
-            ),
+                    ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+                    height: 100.0,
+                    width: 100.0,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Colors.blue,
+                    ),
+                    child: const Align(
+                        alignment: Alignment.centerRight,
+                        child: Text('Container 4')),
+                  ),
+                ),
+              ),
+            ]),
             Column(
               children: [
-                
-              ]
-            ),
-            Column(
-              children: [],
+                Expanded(
+                    flex: 2,
+                    child: Container(
+                        height: 100.0,
+                        width: 100.0,
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.black,
+                            border: Border(
+                              top: BorderSide(
+                                color: Colors.white,
+                                width: 3.0,
+                              ),
+                              bottom: BorderSide(
+                                color: Colors.white,
+                                width: 3.0,
+                              ),
+                              left: BorderSide(
+                                color: Colors.white,
+                                width: 3.0,
+                              ),
+                              right: BorderSide(
+                                color: Colors.white,
+                                width: 3.0,
+                              ),
+                            )),
+                        child: const Center(
+                            child: Text(
+                          'Container 5',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.white),
+                        )
+                      )
+                    )
+                  ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    height: 100.0,
+                    width: 100.0,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Colors.red,
+                    ),
+                    child:
+                        const Text('Con 6', style: TextStyle(fontSize: 30.0)),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
